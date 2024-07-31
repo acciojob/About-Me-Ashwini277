a6f8a01f939984d310e486470060d5504fd2f9a5
Ashwini277 <devarushiashwini@gmail.com>
2024-07-31 23:10:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Your code here.
+// cypress/integration/tests/test.spec.js
+
+describe('About Me Tests', () => {
+    const elementsToCheck = ['Name', 'Email', 'Profile', 'About me', 'Skills'];
+
+    beforeEach(() => {
+        cy.visit('http://localhost:3000'); // Visit your page before each test
+    });
+
+    elementsToCheck.forEach((element) => {
+        it(`Test: ${element} exists`, () => {
+            cy.contains(element).should('be.visible'); // Check visibility
+        });
+    });
+});
+
